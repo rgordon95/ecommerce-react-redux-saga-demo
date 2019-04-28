588b72552484498bfcc80a032f1d38bfa1bac58e
Richard Gordon <richgordon95@gmail.com>
2019-04-28 12:05:24 -0400

    add item quantity handling saga

---
+++ redux-saga-cart/src/sagas/itemQuantitySaga.js
@@ -0,0 +1,54 @@
+import { takeLatest, put, call, select } from 'redux-saga/effects';
+import fetch from 'isomorphic-fetch';
+
+import {
+    INCREASE_ITEM_QUANTITY,
+    DECREASE_ITEM_QUANTITY,
+    setItemQuantityFetchStatus,
+    decreaseItemQuantity,
+    FETCHING,
+    FETCHED
+   } from './../actions';
+   
+import {
+    currentUserSelector
+} from '../selectors';
+
+export function* handleIncreaseItemQuantity({id}) {
+    yield put(setItemQuantityFetchStatus(FETCHING));
+    const user = yield select(currentUserSelector);
+    const response = yield call(fetch, `http://localhost:8081/cart/add/${user.get('id')}/${id}`);
+    console.info("got response:", response);
+
+    if (response.status !== 200) {
+        yield put(decreaseItemQuantity(id, true));
+        alert("sorry, there weren't enough items in stock to complete your request.");
+    }
+
+    yield put(setItemQuantityFetchStatus(FETCHED));
+}
+
+export function* handleDecreaseItemQuantity({id, local}) {
+    if (local) {
+        return;
+    }
+    yield put(setItemQuantityFetchStatus(FETCHING));
+    const user = yield select(currentUserSelector);
+    const response = yield call(fetch, `http://localhost:8081/cart/remove/${user.get('id')}/${id}`);
+    if (response.status !== 200) {
+        console.warn("Received non-200 status:: ", response);
+    }
+    yield put(setItemQuantityFetchStatus(FETCHED));
+}
+
+   export function* itemQuantitySaga() {
+    //    const { user } = yield take(SET_CURRENT_USER);
+    //    const { id } = user;
+    //    const response = yield fetch(`http://localhost:8081/cart/${id}`);
+    //    const { prices } = yield response.json();
+       yield [
+           takeLatest(DECREASE_ITEM_QUANTITY, handleDecreaseItemQuantity),
+           takeLatest(INCREASE_ITEM_QUANTITY, handleIncreaseItemQuantity)
+       ]
+   }
+   
